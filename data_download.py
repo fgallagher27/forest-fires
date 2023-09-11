12d1c571d1d71ca771f2684ccddc360d67dc9d67
Felix Gallagher <94113663+fgallagher27@users.noreply.github.com>
2023-09-11 20:39:28 +0100

    remove commented code

--- data_download.py
+++ data_download.py
@@ -12,7 +12,4 @@ mtbs_folder = os.path.join("data","inputs", "mtbs_wildfires.zip")
 shp_file = "mtbs_perims_DD.shp"
 extract_location = os.path.join("data", "inputs")
 
-# download_zip_from_url(mtbs_url, mtbs_folder)
-# extract_zip(mtbs_folder)
-
 download_data(mtbs_url, mtbs_folder, shp_file, extract_location)
